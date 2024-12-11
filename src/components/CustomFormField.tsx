92c16fad6dd043292b1748304cf253210648a9d2
Yekoko <yekokooo1991@gmail.com>
2024-12-12 00:20:53 +0900

    created home page

---
+++ src/components/CustomFormField.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import Image from "next/image";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldTypes } from "./forms/PatientForm";
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldTypes;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disable?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { placeholder, fieldType, iconSrc, iconAlt } = props;
+  switch (fieldType) {
+    case FormFieldTypes.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc!}
+              alt={iconAlt || "icon"}
+              height={24}
+              width={24}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldTypes.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+          <RenderField field={field} props={props} />
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
