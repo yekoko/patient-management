0474695ffad0fd5037f61cc26c656f97893518cd
Yekoko <yekokooo1991@gmail.com>
2024-12-20 17:44:16 +0900

    creating passKeyModal component and admin dashboard

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ Make sure you have the following installed on your machine:
 
 - [Git](https://git-scm.com/)
 - [Node.js](https://nodejs.org/en)
-- [npm](https://www.npmjs.com/) (Node Package Manager)
+- [yarn](https://yarnpkg.com/) (Package Manager)
 
 **Cloning the Repository**
 
---
+++ src/components/PassKeyModal.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import Image from "next/image";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+} from "./ui/alert-dialog";
+import { AlertDialogTitle } from "@radix-ui/react-alert-dialog";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { decryptKey, encryptKey } from "@/lib/utils";
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "./ui/input-otp";
+
+const PassKeyModal = () => {
+  const [open, setOpen] = useState(false);
+  const [passkey, setPasskey] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const path = usePathname();
+
+  const encryptedKey =
+    typeof window !== undefined
+      ? window.localStorage.getItem("accessKey")
+      : null;
+
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+    if (path) {
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+        setOpen(false);
+        router.push("/admin");
+      } else {
+        setOpen(true);
+      }
+    }
+  }, [encryptedKey]);
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
+
+  const validatePassKey = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      const encryptedKey = encryptKey(passkey);
+      localStorage.setItem("accessKey", encryptedKey);
+      setOpen(false);
+    } else {
+      setError("Invalid passkey. Please try again.");
+    }
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access the admin page, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot index={0} className="shad-otp-slot"></InputOTPSlot>
+              <InputOTPSlot index={1} className="shad-otp-slot"></InputOTPSlot>
+              <InputOTPSlot index={2} className="shad-otp-slot"></InputOTPSlot>
+              <InputOTPSlot index={3} className="shad-otp-slot"></InputOTPSlot>
+              <InputOTPSlot index={4} className="shad-otp-slot"></InputOTPSlot>
+              <InputOTPSlot index={5} className="shad-otp-slot"></InputOTPSlot>
+            </InputOTPGroup>
+          </InputOTP>
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </p>
+          )}
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            onClick={(e) => validatePassKey(e)}
+            className="shad-primary-btn w-full"
+          >
+            Enter Admin Passkey
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+export default PassKeyModal;
